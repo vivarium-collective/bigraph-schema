f71f4bf300d5b33f1132309f0b7b85720c9c7766
Ryan Spangler <ryan.spangler@gmail.com>
2023-05-25 18:28:34 -0700

    working on type parser

--- bigraph_schema/parse.py
+++ bigraph_schema/parse.py
@@ -4,8 +4,8 @@ from parsimonious.nodes import NodeVisitor
 
 parameter_examples = {
     # 'no-parameters': 'simple',
-    'typed': 'a:int|b.(x:length|y:float)',
-    'typed_parameters': 'edge[a:int|b.(x:length|y:float)]',
+    'typed': 'a:field[yellow,tree,snake]|b.(x:length|y:cloud)',
+    'typed_parameters': 'edge[a:int|b.(x:length|y:float),v[zz:float|xx:what]]|okay',
     # 'one-parameter': 'parameterized[A]',
     # 'three-parameters': 'parameterized[A,B,C]',
     # 'nested-parameters': 'nested[outer[inner]]',
@@ -15,14 +15,15 @@ parameter_examples = {
 
 parameter_grammar = Grammar(
     """
-    qualified_type = expression parameter_list? comma?
-    parameter_list = square_left qualified_type+ square_right
-    expression = group / merge / bigraph
+    expression = merge / tree
     merge = bigraph (bar bigraph)+
+    tree = bigraph / type_name
     bigraph = group / nest / control
     group = paren_left expression paren_right
-    nest = control (dot bigraph)+
-    control = symbol (colon qualified_type)?
+    nest = symbol dot bigraph
+    control = symbol colon type_name
+    type_name = symbol parameter_list?
+    parameter_list = square_left expression (comma expression)* square_right
     symbol = ~r"[\w\d-_/*&^%$#@!~`+]+"
     dot = "."
     colon = ":"
@@ -39,37 +40,82 @@ parameter_grammar = Grammar(
 
 
 class ParameterVisitor(NodeVisitor):
-    def visit_qualified_type(self, node, visit):
-        outer_type = visit[0]
-        parameters = []
-        if len(visit[1]['visit']) > 0:
-            parameters = visit[1]['visit'][0]
+    # def visit_qualified_type(self, node, visit):
+    #     outer_type = visit[0]
+    #     parameters = []
+    #     if len(visit[1]['visit']) > 0:
+    #         parameters = visit[1]['visit'][0]
 
-        return [outer_type, parameters]
-
-    def visit_parameter_list(self, node, visit):
-        return visit[1]['visit']
+    #     return [outer_type, parameters]
 
     def visit_expression(self, node, visit):
         import ipdb; ipdb.set_trace()
-        return visit[0]
+        return {
+            'node': node,
+            'visit': visit,
+        }
 
     def visit_merge(self, node, visit):
         import ipdb; ipdb.set_trace()
+        return {
+            'node': node,
+            'visit': visit,
+        }
+
+    def visit_tree(self, node, visit):
+        import ipdb; ipdb.set_trace()
+        return {
+            'node': node,
+            'visit': visit,
+        }
 
     def visit_bigraph(self, node, visit):
         import ipdb; ipdb.set_trace()
-        return visit[0]
+        return {
+            'node': node,
+            'visit': visit,
+        }
 
     def visit_group(self, node, visit):
         import ipdb; ipdb.set_trace()
+        return {
+            'node': node,
+            'visit': visit,
+        }
 
     def visit_nest(self, node, visit):
         import ipdb; ipdb.set_trace()
+        return {
+            'node': node,
+            'visit': visit,
+        }
 
     def visit_control(self, node, visit):
         import ipdb; ipdb.set_trace()
-        return visit[0]
+        return {
+            'node': node,
+            'visit': visit,
+        }
+
+    def visit_type_name(self, node, visit):
+        import ipdb; ipdb.set_trace()
+
+        type_name = visit[0]
+        type_parameters = visit[1]['visit']
+
+        return [type_name, type_parameters]
+
+    def visit_parameter_list(self, node, visit):
+        import ipdb; ipdb.set_trace()
+
+        first_type = [visit[1]['visit'][0]['visit'][0]]
+        rest_types = [
+            inner['visit'][1]['visit'][0]['visit'][0]
+            for inner in visit[2]['visit']]
+
+        parameters = first_type + rest_types
+
+        return parameters
 
     def visit_symbol(self, node, visit):
         return node.text
