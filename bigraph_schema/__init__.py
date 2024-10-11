ff48229d9cdf6c9c434f510db6bd9711e97d317f
Ryan Spangler <ryan.spangler@gmail.com>
2024-10-11 05:34:30 -0700

    working on nested type registration

--- bigraph_schema/__init__.py
+++ bigraph_schema/__init__.py
@@ -1,2 +1,2 @@
-from bigraph_schema.type_system import TypeSystem, Edge
-from bigraph_schema.registry import get_path, establish_path, set_path, transform_path, deep_merge, validate_merge, default
+from bigraph_schema.registry import deep_merge, validate_merge, default
+from bigraph_schema.type_system import TypeSystem, Edge, get_path, establish_path, set_path, transform_path
