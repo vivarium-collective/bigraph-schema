fd822a4d787e86e94b21131434452fc4d5b4b43c
Ryan Spangler <ryan.spangler@gmail.com>
2025-09-22 17:22:30 -0700

    remove everything unnecessary from setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 from setuptools import setup, find_packages
 
 
-# VERSION = '0.0.71'
-
-
 with open("README.md", "r") as readme:
     description = readme.read()
 
 
 setup(
     name="bigraph-schema",
-    # version=VERSION,
-    author="Eran Agmon, Ryan Spangler",
 )
